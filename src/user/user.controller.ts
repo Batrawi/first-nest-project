04fb07d990a6a787cc08cdeecd467edfc56f2b7c
batrawi <batrawi2000forever@gmail.com>
2024-07-26 11:04:39 +0300

    create post rout by using decorator @Post and createUser() method

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
-
+import { Controller, Get, Post } from '@nestjs/common';
 @Controller()
 export class UserController {
   // any rout is a method , every method coming after http method decorator👇
   @Get('user')
   getAllUsers(): string[] {
     return ['Mohammed', 'Anas', 'Mahmoud', 'Ahmed'];
+  }
+  @Post('user')
+  createUser(): string {
+    return 'user created successfully';
   }
 }
