e0c142e54a319f64a5bdf2d8c3085d576cf84e93
batrawi <batrawi2000forever@gmail.com>
2024-07-29 11:49:27 +0300

    Refactor: replace createDTO to CreateDTO , because it is class

--- src/users/DTOs/create.dto.ts
+++ src/users/DTOs/create.dto.ts
@@ -1,4 +1,4 @@
-export class createDTO {
+export class CreateDTO {
   // I will make the id type of string because I will use uuid module to create ids.
   readonly id: string;
   readonly name: string;
