fe5184b810fcb28f9f84c7323eadd1baa64b3849
batrawi <batrawi2000forever@gmail.com>
2024-07-30 11:06:19 +0300

    create update user DTO by using partial types from @nestjs/mapped-types

--- src/users/DTOs/update.dto.ts
+++ src/users/DTOs/update.dto.ts
@@ -1,8 +1,3 @@
-// 😢Sorry, but now I am writing code while I am offline , there is mappedtype that nest provided to do this stuff 👇, by extend the properties and make them optional and its called partial type.
-export class UpdateDTO {
-  readonly id?: string;
-  readonly name?: string;
-  readonly email?: string;
-  readonly age?: number;
-  readonly gender?: string;
-}
+import { CreateDTO } from './create.dto';
+import { PartialType } from '@nestjs/mapped-types';
+export class UpdateDTO extends PartialType(CreateDTO) {}
